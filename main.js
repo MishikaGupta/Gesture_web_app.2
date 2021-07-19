0af18eb02cee6a1c73bb234aeb661fefa86b588e
MishikaGupta <78350675+MishikaGupta@users.noreply.github.com>
2021-07-19 15:52:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+prediction = "";
+
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+
+    Webcam.snap(function(data_uri) {
+
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+    });
+}
+
+console.log("ml5 version -" , ml5.version);
+
+classifier = ml5.imageClassifier("https://storage.googleapis.com/tm-model/cxQNOIjN4/model.json",modelLoaded());
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data ="The predicted gesture is -" + prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
